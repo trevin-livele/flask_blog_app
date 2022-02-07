581efac2bda23130bd222656ef84c01aa1184808
trevin-livele <liveletrevin6@gmail.com>
2022-02-07 21:54:35 +0300

    created login view

--- puppycompanyblog/users/views.py
+++ puppycompanyblog/users/views.py
@@ -24,19 +24,41 @@ def register():
         return redirect(url_for('users.login'))
     return render_template('register.html',form=form)
 
-
-
-
-
-
-
-
-
-
-
-
-
 #login
+@users.route('/login',methods=['GET','POST'])
+def login():
+
+
+    form = LoginForm()
+    if form.validate_on_submit():
+
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user.check_password(form.password.data) and user is not None:
+
+            login_user(user)
+            flash('Log in Success!')
+
+            next = request.args.get('next')
+            if next ==None or not next[0]=='/':
+
+                next == url_for('core.index')
+
+
+            return redirect(next)
+
+    return render_template('login.html',form=form)
+
+
+
+
+
+
+
+
+
+
+
 #logout
 @users.route("/logout")
 def logout():
