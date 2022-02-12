84b62e210642f78a8f2b8bafca6b85a2b0b00e04
trevin-livele <liveletrevin6@gmail.com>
2022-02-12 09:12:04 +0300

    added final commits

--- puppycompanyblog/__init__.py
+++ puppycompanyblog/__init__.py
@@ -12,9 +12,25 @@ app.config['SECRET_KEY'] = 'mysecret'
 ######################################
 ####### DATABASE SETUP ###############
 ######################################
-#'postgresql+psycopg2://moringa:ashihundu@localhost/pitch'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://moringa:ashihundu@localhost/pitch'
+
+#'postgresql+psycopg2://moringa:ashihundu@localhost/trevblog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://moringa:ashihundu@localhost/trevblog'
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
+
+
+
+
+
+
+#'postgresql+psycopg2://moringa:ashihundu@localhost/pitch'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://moringa:ashihundu@localhost/pitch'
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
+
+
+
+
 print(app.config['SQLALCHEMY_DATABASE_URI'],'db not found')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
